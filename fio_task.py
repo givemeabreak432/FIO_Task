ff3ce8c689190ebce5bbe9b5d9373cd9245606a4
givemeabreak432 <givemeabreak432@tfgamessite.com>
2020-01-13 12:17:02 -0700

    prep for additional data input

--- fio_task.py
+++ fio_task.py
@@ -9,15 +9,16 @@ class Reagent:
 	reagentList = []
 	reagentFile = "reagents.txt"
 
-	def __init__(self, name,  quantity, timestamp):
+	def __init__(self, name,  quantity, timestamp, additionalProperties = []):
 		self.name = name
 		self.quantity = quantity
 		self.timestamp = timestamp
-		self.additionalProperties = []
+		self.additionalProperties = additionalProperties
 
 
 	#create new instance of reagent to save to list
-	#TODO clean string (remove delimiters)
+	#TODO clean string (remove delimiters, escape sequences, length)
+	#TODO add duplicate reagent name
 	@staticmethod
 	def newReagent(name, quantity, timestamp):
 		reagent = Reagent(name, quantity, timestamp)
@@ -27,7 +28,6 @@ class Reagent:
 		reagent.saveReagent()
 
 	#startup static method. Reads in reagent file and starts reagentList
-	#TODO additionalProperties storage
 	@staticmethod
 	def startup():
 		try:
@@ -40,7 +40,12 @@ class Reagent:
 		for line in f.read().split("\n"):
 			s = line.split(";");
 			if len(s) > 1:
-				Reagent.reagentList.append(Reagent(s[0], Quantity(s[1], s[2]), s[3]))
+				additionalProperties = []
+				if len(s) > 4: #add additional properties if they exist
+					for i in range(4, len(s), 2):
+						additionalProperties.append([s[i], s[i+1]])
+
+				Reagent.reagentList.append(Reagent(s[0], Quantity(s[1], s[2]), s[3], additionalProperties))
 
 	#returns matchingReagents from reagentList
 	#TODO handle duplicate names
@@ -51,24 +56,29 @@ class Reagent:
 				return each
 
 
-	#saves reagent to text document
+	#saves reagent to text document with no additional properties. 
 	def saveReagent(self):
 		f = open(Reagent.reagentFile, "a")
 		f.write(self.name + ";" + self.quantity.amount + ";" + self.quantity.unit + ";" + self.timestamp + "\n")
 		f.close()
-	#append reagent
-	def appendReagent(self):
-		pass
+
 
 	def toText(self):
 		return "Reagent: " + self.name + "\nQuantity: " + self.quantity.amount + " " + self.quantity.unit + "\nAdded: " + self.timestamp
 
-	#TODO save add additionalProperties to document. 
+	#adds custom property to reagent, appends it to document
 	def addProperty(key, value):
 		self.additionalProperties.append[[key, value]]
 
-	def addProperty(list):
-		self.additionalProperties.append[list[0:1]]
+		f = open(Reagent.reagentFile, "w+")
+		doc = ""
+		for line in f.read().split("\n"):
+			s = line.split(";");
+			if s[0] == self.name:
+				line = line + ";" + key + ";" + value
+			doc = doc + line
+		f.write(doc)
+
 
 #Quantity class for handling units. 
 #Quantity is an amount paired with unit
@@ -93,13 +103,6 @@ class Unit:
 		self.m = m
 		self.b = b
 
-
-
-def main():
-	Reagent.startup()
-
-	#default to input menu
-	drawInput()
 
 #TODO class for drawing/managing window?
 #Window for input of reagents
@@ -137,14 +140,20 @@ def drawReagentProperties(reagent, message = ""):
 
 	window = ui.Window(reagent.name, layout)
 
-	windowManager(window)
+	windowManager(window, reagent)
 
 #update fields for amending reagent data
 def drawReagentUpdate(reagent, message = ""):
-	pass
+	layout = [  [ui.Text(message)],
+				[ui.Text("Enter Property Name", size = (20, 1)), ui.InputText()],
+				[ui.Text("Enter Value", size=(20, 1)), ui.InputText(), ui.InputCombo(Unit.units)],
+	            [ui.Button('Save Property'), ui.Button('Close'), ui.Button('View Reagent')]]
+	window = ui.Window(reagent.name, layout)
+
+	windowManager(window, reagent)
 
 #detects button presses from current window
-def windowManager(window):
+def windowManager(window, reagent = ""):
 	while True:
 
 		#Event Selector
@@ -163,15 +172,27 @@ def windowManager(window):
 	    	drawInput()
 	    	break
 
-	    #find reagent based on reagent name. send it 
+	    #Calls drawReagentProperties based on selected reagent.
+	    #if no selected reagent, checks if there is previously selected reagent and draws that
 	    if event in (None, "View Reagent"):
+	    	if(len(values[0]) != 0):
+	    		reagent = Reagent.searchByName(values[0][0])
+	    	if(isinstance(reagent, Reagent)):
+		    	window.close()
+		    	drawReagentProperties(reagent)
+		    	break
+
+
+	    if event in (None, "Update Reagent"):
 	    	window.close()
-	    	drawReagentProperties(Reagent.searchByName(values[0][0]))
+	    	drawReagentUpdate(reagent)
+	    	break
+
 	    #Amount entered is valid
 	    #saves and timestamps new reagents
 	    if event in (None, "Save Reagent"):
 		    if values[1].isdigit():
-		    	Reagent.newReagent(values[0], values[1], "NA", datetime.now().strftime(timeFormat))
+		    	Reagent.newReagent(values[0], Quantity(values[1], "NA"), datetime.now().strftime(timeFormat))
 		    	text = values[0] + " saved!"
 		    	window.close()
 		    	drawInput(text)
@@ -182,4 +203,7 @@ def windowManager(window):
 		    	drawInput("Entered amount must be a number")
 
 if __name__ == "__main__":
-	main()
+	Reagent.startup()
+
+	#default to input menu
+	drawInput()
